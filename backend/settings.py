756b21585a1a6c6f668af55820d402d2438378be
EhsanMasoudian <ehsan8742@gmail.com>
2025-09-24 15:00:20 +0330

    modified:   .gitignore 	modified:   backend/settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = 'django-insecure-t(_-(ah2@z&t-zsuap4r7iamr7!__o!2^968&^@v=(!!k%g=x2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -133,8 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
